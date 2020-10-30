3282dca847194db639b0ed06b40d1b02cb71ef77
louisnoiz <63070163@kmitl.ac.th>
2020-10-30 17:49:41 +0700

    Add Content and Style

--- index.php
+++ index.php
@@ -4,15 +4,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TJTHANAPAT</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;500&display=swap" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
 </head>
 <body>
-    <h1>My First Page</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    <footer>
+    <div class="container">
+        <h1 class="color-orange">TJTHANAPAT</h1>
+        <h3>Introduction</h3>
         <p>
-            <i>This page is part of studying ITF Academic Year 2020, Faculty of Information Technology, KMITL.</i> Created by TJTHANAPAT
+            Hi! My name is Thanapat Teerarattanyu, or you can call me 'Jay'. 
+            I am a freshmen at Faculty of Information Technology, KMITL. 
+            I am major in Data Science and Business Analytics.
         </p>
-    </footer>
+        <hr>
+        <h3>Contacts</h3>
+        <a href="https://github.com/TJTHANAPAT">Github</a>
+        <a href="https://fb.com/thanapat.teerarattanyu/">Facebook</a>
+        <hr>
+        <h3>Joke</h3>
+        <p>
+            Thai people live in the country which its monarch spends most of his time outside his country.
+        </p>
+        <hr>
+        <h3>My Passions</h3>
+        
+        <img src="https://i.pinimg.com/originals/82/46/9a/82469a43ab1a74e750d5d6e9723e1eb7.jpg" style="width: 100px;">
+        <img src="https://cdn.pixabay.com/photo/2016/09/16/09/20/books-1673578_1280.png" style="width: 100px;">
+
+        
+        
+        <footer>
+            <p>
+                <i>This page is part of studying ITF Academic Year 2020, Faculty of Information Technology, KMITL.</i><br>Created by TJTHANAPAT
+            </p>
+        </footer>
+    </div>
     
 </body>
 </html>
